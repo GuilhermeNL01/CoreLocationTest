468e0cf8754d482226a558a27517d1fd02eb1f8b
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2023-11-14 14:39:38 -0300

    Initial Commit

---
+++ CoreLocationTest/CoreLocationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationTestApp.swift
+//  CoreLocationTest
+//
+//  Created by Guilherme Nunes Lobo on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
